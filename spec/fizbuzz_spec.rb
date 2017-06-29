f3ed7216eba4ac326a2ebb269cdba50a38b895e9
sdavidson140 <sdavidson140@gmail.com>
2017-06-28 23:00:15 -0400

    create RSpec test for fizzbuzz_hash method lab
    close #13

--- spec/fizbuzz_spec.rb
+++ spec/fizbuzz_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../lib/fizzbuzz.rb'
 
 def transform_keys_to_symbols(hash)
@@ -23,7 +25,38 @@ RSpec.describe 'fizzbuzz method' do
   end
 
   it "puts 'fizzbuzz' when number is divisible by 3 and 5" do
-    expected_output = "1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizzbuzz\n"
+    expected_output = "1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\
+\n11\nfizz\n13\n14\nfizzbuzz\n"
     expect { fizzbuzz(15) }.to output(expected_output).to_stdout
+  end
+end
+
+RSpec.describe 'fizzbuzz_hash method' do
+  it 'returns a hash' do
+    result = fizzbuzz_hash(1)
+    expect(result).to be_kind_of(Hash)
+  end
+  it "should add values divisible by 3 to 'fizz' array on hash" do
+    result = fizzbuzz_hash(3)
+    symbol_key_hash = transform_keys_to_symbols(result)
+    expect(symbol_key_hash).to include(fizz: [3])
+  end
+
+  it "should add values divisible by 5 to 'buzz' array on hash" do
+    result = fizzbuzz_hash(5)
+    symbol_key_hash = transform_keys_to_symbols(result)
+    expect(symbol_key_hash).to include(buzz: [5])
+  end
+
+  it "should add values divisible by 3 and 5 to 'fizzbuzz' array on hash" do
+    result = fizzbuzz_hash(15)
+    symbol_key_hash = transform_keys_to_symbols(result)
+    expect(symbol_key_hash).to include(fizzbuzz: [15])
+  end
+
+  it "should add values not divisble by 3 or 5 to 'other' array on hash" do
+    result = fizzbuzz_hash(3)
+    symbol_key_hash = transform_keys_to_symbols(result)
+    expect(symbol_key_hash).to include(other: [1, 2])
   end
 end
