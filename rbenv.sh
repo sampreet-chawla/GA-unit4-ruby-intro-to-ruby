f21bd5c2d3a703b9f47084ad17bf733a44cefdc7
sdavidson140 <sdavidson140@gmail.com>
2017-07-05 08:47:18 -0400

    add script to install rbenv / ruby

---
+++ rbenv.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+if which rvm; then
+  read -r -p "Are you sure you would like to remove rvm? [y/n] " response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+  then
+    rvm implode
+  fi
+fi
+
+#brew install rbenv
+if uname -s != 'Darwin'; then
+  sudo apt-get install libffi-dev
+fi
+
+brew install rbenv
+
+#add to .bashrc
+cat <<'EOF' >> ~/.bashrc
+
+# Rbenv
+if which rbenv > /dev/null; then
+   eval "$(rbenv init -)"
+fi
+
+EOF
+
+source ~/.bashrc
+
+rbenv rehash
+
+if [[ $(uname -s) != 'Darwin' ]]; then
+  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+else
+  brew install ruby-build
+fi
+
+if [[ $(rbenv --version) != 'rbenv 1.1.0' ]]; then
+  echo -e "\n There may have been an issue installing rbenv. Please ask consultant for assistance. \n"
+fi
+
+#TODO prompt for destructive action
+echo bundler > $(rbenv root)/default-gems
+
+rbenv install 2.4.1
+rbenv global 2.4.1
+
+echo 'gem: --no-document' >> ~/.gemrc
